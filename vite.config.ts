55c4cfec81b260f1a80386d76862b650d52e1e62
Andrei Hudovich <andrei.hudovich@gmail.com>
2023-02-16 20:33:30 +0100

    build: add unplugin-vue-components

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import Vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [Vue(), Components()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
