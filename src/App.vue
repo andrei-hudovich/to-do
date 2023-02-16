e82287cc9c41c0ee7781d6c701555d302cf4d365
Andrei Hudovich <andrei.hudovich@gmail.com>
2023-02-16 16:53:43 +0100

    build: set up css processing

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,11 @@
 <template>
   <h1>Welcome to To Do!</h1>
 </template>
+
+<style scoped lang="scss">
+h1 {
+  @include mq($from: container) {
+    display: none;
+  }
+}
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,20 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+            @use 'sass-mq/mq' as * with (
+              $breakpoints: (
+                container: 1120px,
+              )
+            );
+            @use "@/assets/styles/utilities/_functions.scss" as *;
+            @use "@/assets/styles/utilities/_mixins.scss" as *;
+          `
+      }
+    }
   }
 })
